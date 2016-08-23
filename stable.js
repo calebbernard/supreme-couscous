760d32df6098231383ffafe60a9584c604db1095
Caleb <calebbernard@gmail.com>
2016-08-23 04:35:35 -0500

    Fixed all the problems I think

--- stable.js
+++ stable.js
@@ -3,7 +3,7 @@ var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3001);
+app.set('port', 3000);
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

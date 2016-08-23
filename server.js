566949c9f4a078c9fa28fdba0294096097c572f8
Caleb <calebbernard@gmail.com>
2016-08-23 06:16:16 -0500

    .

--- server.js
+++ server.js
@@ -184,6 +184,7 @@ app.get('/check_friend_requests', function(req,res){
       return;
     } else {
       //data.Items.forEach(function(item) {
+        console.log(data.Items[0].friend_requests);
         res.render('check_friend_requests', {sitename: sitename, requests: data.Items[0].friend_requests, logged_in: true, name: name});
         //res.render('success', {sitename: sitename, success_msg: "test", logged_in: true, name: name});
         return;

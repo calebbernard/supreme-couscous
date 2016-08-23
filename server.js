3bdce4fa2f806685d4b7370d10c96cee9c829437
Caleb <calebbernard@gmail.com>
2016-08-23 12:56:03 -0500

    .

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.get('/check_friend_requests', function(req,res){
       res.render('error', {sitename: sitename, error_msg: "Something weird happened with the database.", return_page: return_page});
       return;
     } else {
-        res.render('check_friend_requests', {sitename: sitename, requests_in: data.Items[0].friend_requests_inbox.values, requests_out: data.Items[0].friend_requests_outbox.values, logged_in: true, name: name});
+        res.render('check_friend_requests', {sitename: sitename, requests_in: data.Items[0].friend_request_inbox.values, requests_out: data.Items[0].friend_request_outbox.values, logged_in: true, name: name});
         return;
     }
   });
@@ -263,9 +263,9 @@ app.post('/create_account', function(req,res){
 			  "username":name,
 			  "password":password,
 			  "salt":salt,
-			  "friend_request_inbox",
-			  "friend_request_outbox",
-			  "friends_list"
+			  "friend_request_inbox":[],
+			  "friend_request_outbox":[],
+			  "friends_list":[]
 		  }
 	  };
 	  

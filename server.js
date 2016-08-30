1db4aa6e4276abeb5021241d33ed62b5f69b108a
Caleb <calebbernard@gmail.com>
2016-08-30 16:08:18 -0500

    .

--- server.js
+++ server.js
@@ -597,13 +597,12 @@ app.post('/delete_friend', function(req,res){
     } else {
       // Now we want to hash the given password using the salt in the database.
       console.log(data);
-		  var salt = data.Items.salt;
-		  console.log(data.Items[0].salt);
+		  var salt = data.Items[0].salt;
 		  var hash = crypto.createHash('sha256');
 		  hash.update(pass + salt);
 		  var hashedPassword = hash.digest('hex');
 		  console.log(hashedPassword);
-			if (hashedPassword == data.Items.password){
+			if (hashedPassword == data.Items[0].password){
 			// Delete the account here. Update this as mentioned above.
 				docClient.delete(params, function(err, data) {
 				  if (err){
